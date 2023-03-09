a7195fae31e01e0aa754f1c33f4c9ed79c9db45c
Oleksii Maievskyi <oleksii.maievskyi@dxc.com>
2023-03-09 16:37:59 +0200

    Partially completed Baking Microservice

---
+++ MicroRabbit.Banking.APi/Controllers/BakingController.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.APi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BakingController : ControllerBase
+	{
+		private readonly IAccountService _accountService;
+        public BakingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        //GET api/baking
+        [HttpGet]
+		public ActionResult<IEnumerable<Account>> Get()
+		{
+			return Ok(_accountService.GetAccounts());
+		}
+	}
+}
--- MicroRabbit.Banking.APi/Program.cs
+++ MicroRabbit.Banking.APi/Program.cs
@@ -1,7 +1,24 @@
+using MicroRabbit.Banking.APi.Controllers;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "BankingMicroservice", Version = "v1" }));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+//our DI
+RegisterService(builder.Services);
+
+builder.Services.AddDbContext<BankingDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection"));
+});
+builder.Services.AddMvcCore().AddApiExplorer();
 
 var app = builder.Build();
 
@@ -21,5 +38,14 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1"));
+app.MapControllers();
 app.Run();
+
+
+
+void RegisterService(IServiceCollection services)
+{
+	DependencyContainer.RegisterService(services);
+}
