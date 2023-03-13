f5604af244cf4dbb2d248777b7b2edc63d018b91
Oleksii Maievskyi <oleksii.maievskyi@dxc.com>
2023-03-13 14:44:40 +0200

    MVC project added as presentation layer

---
+++ MicroRabbit.MVC/Controller/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.DTO;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace MicroRabbit.MVC.Controller
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class HomeController : ControllerBase
+	{
+		private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+			_transferService = transferService;   
+        }
+		[HttpPost]
+		public async Task<IActionResult> Transfer(TransferViewModel transferViewModel)
+		{
+			TransferDto transferDto = new TransferDto()
+			{
+				FromAccount = transferViewModel.FromAccount,
+				ToAccount = transferViewModel.ToAccount,
+				TransferAccount = transferViewModel.TransferAmount
+			};
+
+			await _transferService.Transfer(transferDto);
+			return RedirectToAction("Index");
+		}
+	}
+}
